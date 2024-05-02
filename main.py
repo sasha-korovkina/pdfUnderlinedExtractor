ca2a525581f81ad555a83f62a830ed15edb29783
sasha <sasha@cmi2i.com>
2024-05-02 12:14:50 +0100

    done functions

--- main.py
+++ main.py
@@ -1,86 +1,93 @@
 from pdfquery import PDFQuery
 import xml.etree.ElementTree as ET
-from PyPDF2 import PdfWriter, PdfReader
-from reportlab.lib.colors import pink, blue
+from PyPDF2 import PdfReader
 from reportlab.pdfgen import canvas
+import fitz
+import pytesseract
+from PIL import Image
 import io
 
-# Your file path goes here
-file_path = r"C:\Users\sasha\projects\pdfUnderlinedExtractor\loremIpsum.pdf"
+# File path definitions
 xml_path = r"C:\Users\sasha\projects\pdfUnderlinedExtractor\outXML.xml"
-output_pdf_path = r"C:\Users\sasha\projects\pdfUnderlinedExtractor\loremIpsumOut.pdf"
+pdf_path = r"C:\Users\sasha\projects\pdfUnderlinedExtractor\loremIpsum.pdf"
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\sasha\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-pdf = PDFQuery(file_path)
-pdf.load()
-pdf.tree.write(xml_path, pretty_print=True)
-
-tree = ET.parse(xml_path)
-root = tree.getroot()
-
-pdf_reader = PdfReader(file_path)
-pdf_writer = PdfWriter()
-
-all_text = []
 underline_text = []
+words = []
 
-for page in root.findall('.//LTPage'):
-    page_num = int(page.get('page_index'))
-    pdf_page = pdf_reader.pages[page_num]
-    page_height = float(pdf_page.mediabox[3])
+def get_coordinates(pdf_path):
+    pdf = PDFQuery(pdf_path)
+    pdf.load()
+    pdf.tree.write(xml_path, pretty_print=True)
 
-    packet = io.BytesIO()
-    can = canvas.Canvas(packet, pagesize=(pdf_page.mediabox[2], page_height))
-    can.setPageSize((pdf_page.mediabox[2], page_height))
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
 
-    for elem in page.findall('.//*[@bbox]'):
-        bbox = eval(elem.get('bbox'))
-        x0, y0, x1, y1 = map(float, bbox)
-        # can.rect(x0, y0, x1 - x0, y1 - y0, stroke=1, fill=0)
-        text = elem.text.strip() if elem.text else ""
-        text_x = x0
-        text_y = y0
-        can.drawString(text_x, text_y, text)
-        if text:
-            all_text.append([text_x, text_y, x1, y1, text])
+    pdf_reader = PdfReader(pdf_path)
 
-    for elem in page.findall('.//LTRect[@bbox]'):
-        bbox = eval(elem.get('bbox'))
-        x0, y0, x1, y1 = map(float, bbox)
-        can.setStrokeColor(blue)
-        can.setLineWidth(1)
-        can.rect(x0, y0, x1 - x0, y1 - y0)
-        text = elem.text.strip() if elem.text else ""
-        print('found box: ' + str([x0, y0, x1, y1]))
-        underline_text.append([x0, y0, x1, y1])
+    for page in root.findall('.//LTPage'):
+        page_num = int(page.get('page_index'))
+        pdf_page = pdf_reader.pages[page_num]
+        page_height = float(pdf_page.mediabox[3])
 
-    for elem in page.findall('.//LTLine[@bbox]'):  # Adjust this XPath if necessary
-        bbox = eval(elem.get('bbox'))  # Safely parse 'bbox' attribute to get coordinates
-        x0, y0, x1, y1 = map(float, bbox)  # Convert all coordinates to floats
+        packet = io.BytesIO()
+        can = canvas.Canvas(packet, pagesize=(pdf_page.mediabox[2], page_height))
+        can.setPageSize((pdf_page.mediabox[2], page_height))
 
-        can.setStrokeColor(pink)
-        can.setLineWidth(1)  # Set line width, adjust as needed
-        can.line(x0, y0, x1, y1)  # Draw a line from start to end point
+        for elem in page.findall('.//*[@bbox]'):
+            bbox = eval(elem.get('bbox'))
+            x0, y0, x1, y1 = map(float, bbox)
+            # can.rect(x0, y0, x1 - x0, y1 - y0, stroke=1, fill=0)
+            text = elem.text.strip() if elem.text else ""
+            text_x = x0
+            text_y = y0
+            can.drawString(text_x, text_y, text)
 
-    can.save()
-    packet.seek(0)
-    new_pdf = PdfReader(packet)
-    new_page = new_pdf.pages[0]
-    pdf_writer.add_page(new_page)
+        for elem in page.findall('.//LTRect[@bbox]'):
+            bbox = eval(elem.get('bbox'))
+            x0, y0, x1, y1 = map(float, bbox)
+            underline_text.append([x0, y0, x1, y1])
 
-matched_records = []
-collections = []
+    return underline_text
 
-for row in underline_text:
-    print(row)
-    for record in all_text:
-        if round(row[0]) == round(record[0]) and round(row[2]) == round(record[2]):
-            matched_records.append(record)  # Store matched records
+def extract_region_from_pdf(pdf_path, page_number, record):
+    # Open the PDF file
+    doc = fitz.open(pdf_path)
+    page = doc.load_page(page_number)  # page numbering starts from 0
+    page_rect = page.rect
+    y1_coordinate = page_rect.y1
 
-flat_list = [sublist2 for sublist1 in collections for sublist2 in sublist1]
-print(str(flat_list))
-# print(f'Collections are: {flat_list}')
+    y0 = y1_coordinate - record[3] - 10
+    y1 = y1_coordinate - record[3]
+    x0 = record[0]
+    x1 = record[2]
 
-# dataframe_text(all_text, flat_list, results_bene, results_pos, results_isin, results_date, results_owner)
+    coordinates = [x0, y0, x1, y1]
 
-with open(output_pdf_path, 'wb') as output_pdf:
-    pdf_writer.write(output_pdf)
+    # Create a rectangle for the specific area to be extracted
+    clip_rect = fitz.Rect(coordinates)
+
+    pix = page.get_pixmap(clip=clip_rect)
+
+    # Convert the pixmap to an in-memory image
+    img_bytes = io.BytesIO(pix.tobytes("png"))  # Save image to a bytes buffer
+    img = Image.open(img_bytes)
+
+    # Use pytesseract to perform OCR on the image
+    text = pytesseract.image_to_string(img)
+
+    doc.close()
+    return text
+
+page_number = 0
+
+underline_text = get_coordinates(pdf_path)
+
+page_number = int(input('Please enter the page number you need (starting at 0): '))
+
+for record in underline_text:
+    extracted_text = extract_region_from_pdf(pdf_path, page_number, record)
+    cleaned_text = extracted_text.replace('\n', '')
+    words.append(cleaned_text)
+
+print(words)
--- tests/test_pdfCordCutting.py
+++ tests/test_pdfCordCutting.py
@@ -1,40 +1,41 @@
 import fitz
+import pytesseract
+from PIL import Image
+import io
 
-def extract_region_from_pdf(pdf_path, page_number, rect, output_path):
+def extract_region_from_pdf(pdf_path, page_number, record):
+    # Open the PDF file
     doc = fitz.open(pdf_path)
     page = doc.load_page(page_number)  # page numbering starts from 0
     page_rect = page.rect
-    print("Page dimensions:", page_rect)
-    clip_rect = fitz.Rect(rect)
+    y1_coordinate = page_rect.y1
+
+    y0 = y1_coordinate - record[3] - 10
+    y1 = y1_coordinate - record[3]
+    x0 = record[0]
+    x1 = record[2]
+
+    coordinates = [x0, y0, x1, y1]
+
+    # Create a rectangle for the specific area to be extracted
+    clip_rect = fitz.Rect(coordinates)
+
     pix = page.get_pixmap(clip=clip_rect)
-    pix.save(output_path)
+
+    # Convert the pixmap to an in-memory image
+    img_bytes = io.BytesIO(pix.tobytes("png"))  # Save image to a bytes buffer
+    img = Image.open(img_bytes)
+
+    # Use pytesseract to perform OCR on the image
+    text = pytesseract.image_to_string(img)
+
     doc.close()
+    print(text)
+    return text
 
 pdf_path = r"C:\Users\sasha\projects\pdfUnderlinedExtractor\loremIpsum.pdf"
-output_image_path = r"C:\Users\sasha\projects\pdfUnderlinedExtractor\loremIpsumImgTest.png"
-
-# found box: [107.57, 757.9, 139.97, 758.14]
-# found box: [180.31, 724.06, 215.854, 724.3]
-# found box: [410.33, 689.95, 523.42, 690.19]
-# found box: [181.75, 622.97, 224.734, 623.21]
-# found box: [281.88, 589.13, 333.984, 589.37]
-# found box: [252.82, 538.22, 282.604, 538.46]
-# found box: [349.58, 538.22, 387.044, 538.46]
-# found box: [361.1, 521.18, 403.364, 521.42]
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\sasha\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
 record = [281.88, 589.13, 333.984, 589.37]
-
-y0 = 841.9199829101562 - record[3] - 10
-y1 = 841.9199829101562 - record[3]
-x0 = record[0]
-x1 = record[2]
-
-coordinates = [x0, y0, x1, y1]
-print(coordinates)
-#coordinates = [107.57, 757.9, 139.97, 758.14]  # [x0, y0, x1, y1]
-page_number = 0  # change as per your PDF
-
-# Call the function with the specified parameters
-extract_region_from_pdf(pdf_path, page_number, coordinates, output_image_path)
-
-print("Image saved at:", output_image_path)
+page_number = 0
+extract_region_from_pdf(pdf_path, page_number, record)
